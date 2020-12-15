aac272031469d91d90efe1219cf189147431630c
Maarten Beeckmans <maartenb@inuits.eu>
2020-12-15 19:42:04 +0100

    add missing port env variable

--- manifests/init.pp
+++ manifests/init.pp
@@ -69,7 +69,7 @@ class profile_consul (
   file { '/etc/profile.d/consul.sh':
     ensure  => file,
     mode    => '0644',
-    content => "export CONSUL_HTTP_ADDR=https://${facts['networking']['ip']}\nexport CONSUL_CACERT=${root_ca_file}",
+    content => "export CONSUL_HTTP_ADDR=https://${facts['networking']['ip']}:8500\nexport CONSUL_CACERT=${root_ca_file}",
   }
   create_resources(consul::check, $checks)
   create_resources(consul::service, $services)
