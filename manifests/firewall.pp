a922d4334c3808450386227b558fbfd829459e3d
Maarten Beeckmans <maartenb@inuits.eu>
2020-12-22 17:20:40 +0100

    remove join wan from firewall

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -6,7 +6,6 @@ class profile_consul::firewall (
   String              $connect_sidecar_port_range = $::profile_consul::connect_sidecar_port_range,
   Boolean             $server                     = $::profile_consul::server,
   Boolean             $ui                         = $::profile_consul::ui,
-  Optional[String[1]] $join_wan                   = $::profile_consul::join_wan,
 ) {
   firewall { '08600 allow consul DNS TCP':
     dport  => 8600,
@@ -37,12 +36,6 @@ class profile_consul::firewall (
     firewall { '08301 allow consul serf LAN':
       dport  => 8301,
       action => accept,
-    }
-    if $join_wan {
-      firewall { '08302 allow consul serf WAN':
-        dport  => 8302,
-        action => 'accept',
-      }
     }
     if $ui {
       firewall { '08500 allow consul ui':
