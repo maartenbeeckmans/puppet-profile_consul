b4034cfa1965adf015378498e3b6c07515c1a2ec
Maarten Beeckmans <maartenb@inuits.eu>
2021-05-11 12:58:42 +0200

    use puppet certs for consul

--- manifests/certs.pp
+++ manifests/certs.pp
@@ -2,13 +2,14 @@
 #
 #
 class profile_consul::certs (
-  Stdlib::Absolutepath $root_ca_file = $::profile_consul::root_ca_file,
-  Stdlib::Absolutepath $cert_file    = $::profile_consul::cert_file,
-  Stdlib::Absolutepath $key_file     = $::profile_consul::key_file,
-  Stdlib::Absolutepath $certs_dir    = $::profile_consul::certs_dir,
-  Optional[String]     $root_ca_cert = $::profile_consul::root_ca_cert,
-  Optional[String]     $consul_cert  = $::profile_consul::consul_cert,
-  Optional[String]     $consul_key   = $::profile_consul::consul_key,
+  Stdlib::Absolutepath $root_ca_file     = $::profile_consul::root_ca_file,
+  Stdlib::Absolutepath $cert_file        = $::profile_consul::cert_file,
+  Stdlib::Absolutepath $key_file         = $::profile_consul::key_file,
+  Stdlib::Absolutepath $certs_dir        = $::profile_consul::certs_dir,
+  Boolean              $use_puppet_certs = $::profile_consul::use_puppet_certs,
+  Optional[String]     $root_ca_cert     = $::profile_consul::root_ca_cert,
+  Optional[String]     $consul_cert      = $::profile_consul::consul_cert,
+  Optional[String]     $consul_key       = $::profile_consul::consul_key,
 ) {
   file { $certs_dir:
     ensure => directory,
@@ -19,16 +20,31 @@ class profile_consul::certs (
     mode   => '0644',
     notify => Service['consul'],
   }
-  file { $root_ca_file:
-    ensure  => present,
-    content => $root_ca_cert,
-  }
-  file { $cert_file:
-    ensure  => present,
-    content => $consul_cert,
-  }
-  file { $key_file:
-    ensure  => present,
-    content => $consul_key,
+  if $use_puppet_certs {
+    file { $root_ca_file:
+      ensure => present,
+      source => $facts['extlib.puppet_config']['localcacert'],
+    }
+    file { $cert_file:
+      ensure => present,
+      source => $facts['extlib.puppet_config']['hostcert'],
+    }
+    file { $key_file:
+      ensure => present,
+      source => $facts['extlib.puppet_config']['hostprivkey'],
+    }
+  } else {
+    file { $root_ca_file:
+      ensure  => present,
+      content => $root_ca_cert,
+    }
+    file { $cert_file:
+      ensure  => present,
+      content => $consul_cert,
+    }
+    file { $key_file:
+      ensure  => present,
+      content => $consul_key,
+    }
   }
 }
